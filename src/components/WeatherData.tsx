387eb3bdefb855a01f61a3d47235b7c502a9eab8
Andreas Halvorsen Tollånes <andreas@tollanes.com>
2020-10-04 17:30:50 +0200

    added random image to the weather background using unsplash

--- src/components/WeatherData.tsx
+++ src/components/WeatherData.tsx
@@ -1,9 +1,39 @@
-import React from "react";
-import "../App.scss";
+import React, { useEffect, useState } from "react";
+import Unsplash, { toJson } from "unsplash-js";
+import "../styles/weather.scss";
+
+const IMAGE_API_KEY = process.env.REACT_APP_IMAGE_API_KEY;
+
+const unsplash = new Unsplash({
+  accessKey: IMAGE_API_KEY,
+});
 
 // TODO: make this an interface
 function WeatherData(props: any) {
-  return <div className="weather-container">{props.weather.timezone}</div>;
+  const [backgroundImage, setBackgroundImage] = useState("");
+
+  useEffect(() => {
+    unsplash.photos
+      .getRandomPhoto({ collections: [327760] })
+      .then(toJson)
+      .then((res) => {
+        console.log("res: ", res);
+        setBackgroundImage(res.urls.regular);
+      });
+  }, []);
+
+  return (
+    <div
+      className="weather-container"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundColor: "#303871",
+      }}
+    >
+      <div className="overlay"></div>
+      Potato
+    </div>
+  );
 }
 
 export default WeatherData;
